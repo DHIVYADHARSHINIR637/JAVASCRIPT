49dcb2d66749599ef725394c83d70e295e5be001
DHIVYADHARSHINIR637 <dhivyadharshinid589@gmail.com>
2025-07-16 22:24:32 +0530

    Create hi.js

---
+++ hi.js
@@ -0,0 +1,8 @@
+console.log("hello world"); 
+console.log("hello dhivya");
+console.log("how are you");
+//let bookName;
+bookName="harrey petter";
+let authorName="j.k.rowling",price=200;
+console.log(bookName);
+console.log(authorName);
