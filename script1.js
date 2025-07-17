50d234cad97f66b969970ba541dc8753ac4b4036
DHIVYADHARSHINIR637 <dhivyadharshinid589@gmail.com>
2025-07-17 22:50:52 +0530

    Create script1.js

---
+++ script1.js
@@ -0,0 +1,9 @@
+const name="dhivya";
+const num=10;
+const UNAME="dhivy123@gmail.com";
+const PASSWORD="sdhfhrggt";
+Login(UNAME,PASSWORD);
+const DOB="25/07/2005";
+const AGE=20;
+console.log(DOB);
+console.log(AGE);//Login(UNAME,PASSWORD);
