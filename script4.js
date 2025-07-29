14697e3244be7beff011de9fece86352370141a0
DHIVYADHARSHINIR637 <dhivyadharshinid589@gmail.com>
2025-07-29 21:15:54 +0530

    Create script4.js

---
+++ script4.js
@@ -0,0 +1,16 @@
+let intro="hi this is dhivya";
+console.log(intro);
+let hello="this is dhivya's home";
+console.log(hello);
+let quote='he said"i am bat man"';
+console.log(quote);
+let paragraph=`this is bat man's home.bruce wayne said "i am bat man"`;
+console.log(paragraph);
+let embed=`1+2=${1+2}`;
+console.log(embed);
+let num=`123+456=${123+456}`;
+console.log(num);
+let name="dhivya";
+let sentence=`My name is ${name}`;
+console.log(sentence);
+  
