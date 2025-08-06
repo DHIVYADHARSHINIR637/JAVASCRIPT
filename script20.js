b29eec0d2cfadf7e9abb7ee27198ea88467b85f2
DHIVYADHARSHINIR637 <dhivyadharshinid589@gmail.com>
2025-08-06 22:15:16 +0530

    Create script20.js

---
+++ script20.js
@@ -0,0 +1,14 @@
+let op="+";
+let a=10;
+let b=12;
+switch(op)
+{
+    case "+":
+    console.log(a+b);
+    break;
+    case "-":
+    console.log(-b);
+    break;
+    default:
+        console.log("invalid data");
+    }
