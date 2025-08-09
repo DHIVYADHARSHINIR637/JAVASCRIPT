cdc42069098af4fafba384e8c661e0ff7ad71241
DHIVYADHARSHINIR637 <dhivyadharshinid589@gmail.com>
2025-08-09 21:38:16 +0530

    Create script26.js

---
+++ script26.js
@@ -0,0 +1,17 @@
+let car={
+    name:"nexon",
+    brand:"tata",
+    price:850000,
+    engine:"petrol",
+    roadPrice(price)
+    {
+        return price+2;
+    }
+
+};
+car['model year']=21024;
+console.log(car);
+console.log(car.name);  
+console.log(car.price);
+console.log(car); 
+console.log(car.roadPrice(11));
