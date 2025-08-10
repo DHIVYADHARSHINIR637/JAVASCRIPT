770ce1f5262862030733f74c1b764c3ae9668524
DHIVYADHARSHINIR637 <dhivyadharshinid589@gmail.com>
2025-08-10 21:44:08 +0530

    Create constructo1r.js

---
+++ constructo1r.js
@@ -0,0 +1,13 @@
+let a={
+    name:'dhivya',
+};
+function User()
+{
+    this .name='dhivya';
+    this.age=function()
+    {
+        return 23;
+    }
+}
+let b=new User();
+console.log(b.age());
