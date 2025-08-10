b1aa3a915909c1540ec72b952118b8fb32051298
DHIVYADHARSHINIR637 <dhivyadharshinid589@gmail.com>
2025-08-10 21:48:47 +0530

    Create dattime.js

---
+++ dattime.js
@@ -0,0 +1,7 @@
+let day=new Date('2005-07-25');
+console.log(day);
+console.log(day.getDate());
+console.log(day.getDay());
+console.log(day.getFullYear());
+console.log(day.getHours());
+console.log(day.getMonth());
